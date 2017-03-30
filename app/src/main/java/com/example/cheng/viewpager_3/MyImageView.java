f0dc4d73f2ca81bb8b165ea782aa5de055e7748a
chengxiaobo2 <1003153190@qq.com>
2017-03-30 16:19:24 +0800

    2_3 viewpager horizontalScroll and vetical Scroll for example jinritoutiao app. fix touchEvent

--- app/src/main/java/com/example/cheng/viewpager_3/MyImageView.java
+++ app/src/main/java/com/example/cheng/viewpager_3/MyImageView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
 import android.widget.ImageView;
 
 /**
@@ -12,9 +11,6 @@ import android.widget.ImageView;
  */
 
 public class MyImageView extends ImageView {
-
-    private boolean isIntercept=false;
-    private boolean isFirstMove=true;
 
     private float lastX=0.0f;
     private float lastY=0.0f;
@@ -26,29 +22,19 @@ public class MyImageView extends ImageView {
     private float parentDownX=0.0f;
     private float parentDownY=0.0f;
 
-    private int mTouchSlop;
-
     public MyImageView(Context context)
     {
         super(context);
-        init(context);
     }
 
     public MyImageView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        init(context);
     }
 
     public MyImageView(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
-        init(context);
-    }
-
-    public void init(Context context)
-    {
-        mTouchSlop = ViewConfiguration.get(context).getScaledPagingTouchSlop();
     }
 
     @Override
@@ -78,8 +64,6 @@ public class MyImageView extends ImageView {
         {
             case MotionEvent.ACTION_DOWN:
 
-                isIntercept=false;
-
                 downX=event.getX();
                 downY=event.getY();
 
@@ -98,93 +82,38 @@ public class MyImageView extends ImageView {
 
                 System.out.println("====ACTION_MOVE==last====x===="+lastX+"==Y=="+lastY);
 
-                if(isFirstMove)
+                if((moveX!=downX||moveY!=downY))
                 {
+                    System.out.println("====intercept==ACTION_MOVE==========");
 
-                    if((moveX!=downX||moveY!=downY)&&(Math.abs(moveX - downX)>mTouchSlop*2||Math.abs(moveY - downY)>mTouchSlop*2))
-                    {
-                        isFirstMove = false;
-
-                        if (Math.abs(moveX - downX) < Math.abs(moveY - downY)) {
-                            System.out.println("====intercept==ACTION_MOVE==========");
-
-                            this.getParent().requestDisallowInterceptTouchEvent(true);
-
-                            isIntercept = true;
-
-                            translate();
-
-                            return true;
-                        } else {
-                            this.getParent().requestDisallowInterceptTouchEvent(false);
-                            System.out.println("====nointercept==ACTION_MOVE==========");
-
-                            return false;
-                        }
-                    }
-
-                }else
-                {
-                    if(isIntercept)//上次拦截
-                    {
-
-                        System.out.println("===next=intercept==ACTION_MOVE==========");
-
-                        translate();
-                        return true;
-
-                    }else
-                    {
-                        System.out.println("===next=nointercept==ACTION_MOVE==========");
-                        return false;
-                    }
+                    translate();
                 }
 
-
+                break;
 
             case MotionEvent.ACTION_UP:
 
                 System.out.println("====ACTION_UP==========");
-                isIntercept=false;
-                isFirstMove=true;
-
-                this.setTranslationX(0);
-                this.setTranslationY(0);
-
-                this.invalidate();
-
-                this.getParent().requestDisallowInterceptTouchEvent(false);
-
-                break;
 
             case MotionEvent.ACTION_CANCEL:
 
                 System.out.println("====ACTION_CANCEL==========");
-                isIntercept=false;
-                isFirstMove=true;
 
-                this.getParent().requestDisallowInterceptTouchEvent(false);
+                this.setTranslationX(0);
+                this.setTranslationY(0);
+                this.invalidate();
+
                 break;
 
             default:
                 break;
         }
 
-        return false;
+        return true;
     }
 
     private void translate()
     {
-//        ObjectAnimator objectAnimatorX=ObjectAnimator.ofFloat(this,"translationX",0,lastX-downX);
-//        ObjectAnimator objectAnimatorY=ObjectAnimator.ofFloat(this,"translationY",0,lastY-downY);
-//        objectAnimatorX.setDuration(100);
-//        objectAnimatorY.setDuration(100);
-//        objectAnimatorX.start();
-//        objectAnimatorY.start();
-
-//        this.setTranslationX(10/        this.setTranslationY(100);
-
-
         this.setTranslationX(parentLastX-parentDownX);
         this.setTranslationY(parentLastY-parentDownY);
 
